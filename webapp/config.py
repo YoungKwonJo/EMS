c0c7637cf0f365a1719cfb9fc35d8a916895d2f3
alden.kang <alden.kang@aldenkangui-MacBook-Pro.local>
2018-03-19 15:26:01 +0900

    first commit

--- webapp/config.py
+++ webapp/config.py
@@ -8,29 +8,27 @@ class Config:
 
 class DevelopmentConfig(Config):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = "mysql://rackmonkey:sksmsrktnek@infra-devdb.is.daumkakao.io/rackmonkey"
+	SQLALCHEMY_DATABASE_URI = "mysql://username:passwd@your_development_db/database_name"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	GRAFANA_HOST="infra-devdb.is.daumkakao.io:3000"
+	GRAFANA_HOST="your_development_grafana_url:3000"
 
-	INFLUXDB_HOST = "infra-devdb.is.daumkakao.io"
+	INFLUXDB_HOST = "your_development_influxdb"
 	INFLUXDB_PORT = 8086
 	INFLUXDB_USERNAME = "root"
 	INFLUXDB_PASSWORD = "root"
 	INFLUXDB_DATABASE = "ems_elasticsearch_monitoring"
-
 
 class ProductionConfig(Config):
 	DEBUG = False
-	SQLALCHEMY_DATABASE_URI = "mysql://rackmonkey:sksmsrktnek@infradb01.mydb.iwilab.com/rackmonkey"
+	SQLALCHEMY_DATABASE_URI = "mysql://username:passwd@your_production_db/database_name"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	GRAFANA_HOST="infra-devdb.is.daumkakao.io:3000"
+	GRAFANA_HOST="your_production_grafana_url:3000"
 
-	INFLUXDB_HOST = "infra-devdb.is.daumkakao.io"
+	INFLUXDB_HOST = "your_production_influxdb"
 	INFLUXDB_PORT = 8086
 	INFLUXDB_USERNAME = "root"
 	INFLUXDB_PASSWORD = "root"
 	INFLUXDB_DATABASE = "ems_elasticsearch_monitoring"
-
 
 config = {
 	'development' : DevelopmentConfig,
